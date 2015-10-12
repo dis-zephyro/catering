ce37a62440b828fbfd449702af1322acf2b95cc7
dis-zephyro <zephyro@yandex.ru>
2015-10-13 00:01:47 +0300

    v1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
 $('.portfolio__gallery').tabs();
+
+
+//  Slider
+
+$('.reply').slick({
+    arrows: false,
+    autoplay: true,
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1
+});
+
+$('.reply__nav.prev').click(function(){
+    $('.reply').slick('slickPrev');
+});
+
+$('.reply__nav.next').click(function(){
+    $('.reply').slick('slickNext');
+});
