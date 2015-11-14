a9ca2d90f7dea8885d475a23266b528d3df145e3
dis-zephyro <zephyro@yandex.ru>
2015-11-15 02:40:06 +0300

    v1

---
+++ index-modx.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html class="no-js" lang="ru">
+    <head>
+        [[$Head]]
+    </head>
+    
+    <body>
+    [[$Block01]]
+    [[$Block02]]
+    [[$Block03]]
+    [[$Block04]]
+    [[$Block05]]
+    [[$Block06]]
+    [[$Block07]]
+    [[$Block08]]
+    [[$Block09]]
+    [[$Block10]]
+
+    [[$Popup]]
+    [[$Footer]]
+
+    </body>
+</html>
